ea064bc49ec68455ce188d2c8bbe28435728589d
Mohammadjavad Habibi <hjavad257@gmail.com>
2025-07-18 12:58:12 -0700

    i started my first react project

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + mohammadjavad</h1>
+      <h1>Vite + React + let's go</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
